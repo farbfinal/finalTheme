398dfaaa541f844e4c38c9a976154963061fe1a5
farbfinal <info@farbfinal.de>
2017-04-24 16:05:26 +0200

    v1.0.1

--- src/Containers/finalTheme.php
+++ src/Containers/finalTheme.php
@@ -3,6 +3,6 @@ namespace finalTheme\Containers;
 use Plenty\Plugin\Templates\Twig;
 class finalTheme {
     public function call(Twig $twig):string {
-        return $twig->render('finalTheme::finalTheme');
+        return $twig->render('finalTheme::content.Theme');
     }
 }
