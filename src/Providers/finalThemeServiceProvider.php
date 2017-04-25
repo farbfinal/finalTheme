1f209f21432d0877627a29c22b8137cee92f01bf
farbfinal <info@farbfinal.de>
2017-04-25 15:33:07 +0200

    v1.0.4

--- src/Providers/finalThemeServiceProvider.php
+++ src/Providers/finalThemeServiceProvider.php
@@ -1,7 +1,6 @@
 <?php
 namespace finalTheme\Providers;
 use IO\Extensions\Functions\Partial;
-use IO\Helper\TemplateContainer;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
@@ -10,14 +9,8 @@ class finalThemeServiceProvider extends ServiceProvider {
 
 	}
 	public function boot(Twig $twig, Dispatcher $eventDispatcher) {
-		$eventDispatcher->listen('tpl.basket', function(TemplateContainer $container, $templateData) {
-			$container->setTemplate('finalTheme::content.ThemeBasket');
-		}, 0);
 		$eventDispatcher->listen('IO.init.templates', function(Partial $partial) {
 		   $partial->set('header', 'finalTheme::content.ThemeHeader');
-		}, 0);
-		$eventDispatcher->listen('IO.init.templates', function(Partial $partial) {
-		   $partial->set('navigation', 'finalTheme::content.ThemeNavigation');
 		}, 0);
 		$eventDispatcher->listen('IO.init.templates', function(Partial $partial) {
 		   $partial->set('footer', 'finalTheme::content.ThemeFooter');
